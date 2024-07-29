c72512d7313ccaab19bbad516bb0abcf8a830ca0
Gordon Julian Koehn <gordon@koehn.net>
2024-07-29 10:57:19 +0200

    add infurua network config

--- src/riskcraft/ocean.py
+++ src/riskcraft/ocean.py
@@ -1,7 +1,23 @@
 """Ocean module for RiskCraft."""
 
+import json
+
+env = json.load(open(".env.json"))
+
 
 # TODO: remove this test function
 def foo(x: int, y: int) -> int:
     """Add two numbers together."""
     return x + y
+
+
+# main
+def main():
+    """Main function."""
+    print(foo(1, 2))
+
+    print(env.get("INFURA_NODE_ENDPOINT"))
+
+
+if __name__ == "__main__":
+    main()
